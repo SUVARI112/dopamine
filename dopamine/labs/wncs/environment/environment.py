6cd8a1603d9ec7273a9655d49c1c9fcb8bbbdb06
SUVARI112 <yusuf.suvari.tr@gmail.com>
2025-01-07 02:10:30 +0100

    Update cost type and algorithm in full_rainbow.gin; modify state_cost calculation in environment.py

--- dopamine/labs/wncs/environment/environment.py
+++ dopamine/labs/wncs/environment/environment.py
@@ -195,7 +195,7 @@ class Environment():
         return cost
 
     def state_cost(self, state):
-        return np.sum(np.pow(state, 3))
+        return np.sum(np.pow(state, 2))
 
     @staticmethod
     def calculate_correct_differences(arr1, arr2):
